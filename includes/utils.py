6b4d624ac6b09627e7e89b12eaaaade1337e076e
Omar Abdel-Wahab <omar.abdelwahab@egyptdc.com>
2009-12-20 02:00:54 +0200

    Utility library for BAST

---
+++ includes/utils.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+class output:
+    """
+    Send a message to stdin, email, etc...
+    """
+    def to_shell(self):
+        """
+        Handle coloured output to BASH
+        """
+        colors = {'bold': '\x1b[1m', 
+                  'normal':'\x1b[0m',
+                  'blue':'\x1b[34m',
+                  'green':'\x1b[32m',
+                  'red':'\x1b[31m',
+                  'cyan':'\x1b[36m'
+                  }
+        if (os.environ['SHELL'] != '/bin/bash' or (self.params['bold'] == False and self.params['color'] == '')):
+            sys.stdout.writelines(self.message)
+        else:
+            text = ""
+            if "bold" in self.params and self.params['bold'] == True:
+                text += "%(bold)s" % colors
+            for key in colors:
+                if key == color:
+                    text += colors[key]
+            text += self.message
+            text += "%(normal)s" % colors
+            sys.stderr.writelines(text)
+        return 0
+    
+    def to_email(self):
+        """
+        Handle output to e-mails
+        """
+        
