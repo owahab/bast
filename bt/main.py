01881aadc152408b738fef5e0bb95608909db266
Omar Abdel-Wahab <owahab@gmail.com>
2010-11-21 10:15:01 +0200

    Various fixes: minor code refactoring, adding symblic link handling, moving log to project directory

--- bt/main.py
+++ bt/main.py
@@ -13,30 +13,37 @@ class bast(object):
         """
         Initializing
         """
+        # Initialize command-line options
         self.__init_options()
-        project_name = os.path.splitext(os.path.basename(self.conf))[0]
-        # Logging
-        self.__init_logging(project_name)
-        self.__init_config()
+        # Define project name
+        self.project_name = os.path.splitext(os.path.basename(self.conf))[0]
+        # Initialize config
+        if not self.__init_config():
+          # Exit
+          return
+        
+        # Define project root directory
+        self.project_dir = '%s/%s' %(self.config.get('BAST', 'root', '/var/backup'), self.project_name)
+        # Now create it
+        if not os.path.exists(self.project_dir):
+          os.mkdir(self.project_dir)
+        # Initialize logging
+        self.__init_logging()
+        # Initialize process name
         self.__init_proc_name()
-        # TODO: make this path thingy a little smarter
-        self.__init_path()
+        # Initialize path settings
+        self.__init_path()  
         
         report = []
         if self.config.getboolean('BAST', 'backups', True):
-          self.log.info("Starting backup for %s..." % project_name)
+          self.log.info("Starting backup for %s..." % self.project_name)
           # Backup IDs use this pattern: <project-name>--<yyyy.mm.dd>-<hh.mm.ss>
           now = datetime.datetime.now()
           # It is a requirement that project name doesn't contain any non-alphanumeric
           # characters, maybe we should validate that here.
           now_tag = '%04d.%02d.%02d-%02d.%02d.%02d' % (now.year, now.month, now.day, now.hour, now.minute, now.second)
-          backup_id = '%s--%s' % (project_name, now_tag)
-          destination = self.config.get('BAST', 'root', '/var/backup')
-          project_dir = '%s/%s' %(destination, project_name)
-          target = '%s/%s' % (project_dir, now_tag)
-          if not os.path.exists(project_dir):
-            self.log.debug('Creating directory %s.' % project_dir)
-            os.mkdir(project_dir)
+          backup_id = '%s--%s' % (self.project_name, now_tag)
+          target = '%s/%s' % (self.project_dir, now_tag)
           self.log.debug('Creating directory %s.' % target)
           os.mkdir(target)
           self.log.debug('Changing directory to %s.' % target)
@@ -56,8 +63,8 @@ class bast(object):
             s = 'Executed plugin %s: %s' % (k, ('OK' if v == True else 'Failed'))
             report.append(s)
             self.log.debug(s)
-          self.log.debug('Changing directory to %s.' % project_dir)
-          os.chdir(project_dir)
+          self.log.debug('Changing directory to %s.' % self.project_dir)
+          os.chdir(self.project_dir)
           # Compress
           self.log.debug('Compressing backup directory %s.' % now_tag)
           backup_file = self.compress(now_tag)
@@ -68,13 +75,13 @@ class bast(object):
           self.log.info('Backup complete!')
           backup_status = 'Successful: %s' % now_tag
         else:
-          self.log.info('Backups for %s are suspended by configuration file. Exiting!' % project_name)
+          self.log.info('Backups for %s are suspended by configuration file. Exiting!' % self.project_name)
           backup_status = 'Suspended'
         # Rotate old backups
-        report.append(self.rotate(self.config.getint('BAST', 'rotate', 10), project_name, project_dir))
+        report.append(self.rotate(self.config.getint('BAST', 'rotate', 10)))
         # Send notifications
         if self.config.getboolean('BAST', 'notifications', False):
-          self.notify(project_name, backup_status, report)
+          self.notify(backup_status, report)
 
     def __init_path(self):
         self.log.debug('Initializing path settings.')
@@ -83,8 +90,8 @@ class bast(object):
         if not os.path.exists(os.path.join(basedir, "bast.py")):
             cwd = os.getcwd()
             if os.path.exists(os.path.join(cwd, "bast.py")):
-                self.basedir = cwd
-        sys.path.insert(0, self.basedir)
+                basedir = cwd
+        sys.path.insert(0, basedir)
 
     def __init_options(self):
         from optparse import OptionParser
@@ -100,12 +107,12 @@ class bast(object):
         else:
             self.conf = self.args[0]
         
-    def __init_logging(self, project_name):
+    def __init_logging(self):
       import logging
       logging.basicConfig(level=logging.DEBUG,
         format="[%(asctime)s] [%(levelname)-8s] %(message)s",
         datefmt='%a %b %d %Y %H:%M:%S',
-        filename='log/%s.log' % project_name
+        filename='%s/%s.log' % (self.project_dir, self.project_name)
       )
       console = logging.StreamHandler()
       # formatter = logging.Formatter('%(message)s')
@@ -115,19 +122,23 @@ class bast(object):
       if self.options.debug:
         console.setLevel(logging.DEBUG)
         console.setFormatter(logging.Formatter('[%(levelname)-8s] %(message)s'))
-        logging.getLogger(project_name).addHandler(console)
+        logging.getLogger(self.project_name).addHandler(console)
       elif self.options.verbose:
         console.setLevel(logging.INFO)
-        logging.getLogger(project_name).addHandler(console)
-      self.log = logging.getLogger(project_name)
+        logging.getLogger(self.project_name).addHandler(console)
+      self.log = logging.getLogger(self.project_name)
       self.log.debug("Initializing BAST %s" % __version__)
 
     def __init_config(self):
-      from bt import common
-      self.log.debug("Loading configuration file %s." % self.conf)
-      # Read configuration file
-      self.config = common.MyConfigParser()
-      self.config.read(self.conf)
+      if not os.path.exists(self.conf):
+        print 'Unable to access config file: %s.' % self.conf
+        return False
+      else:
+        from bt import common
+        # Read configuration file
+        self.config = common.MyConfigParser()
+        self.config.read(self.conf)
+        return True
 
     def __init_proc_name(self):
       if sys.platform == 'linux2':
@@ -186,9 +197,9 @@ class bast(object):
       return name    
       
     
-    def rotate(self, count, project_name, directory):
-      self.log.debug('Rotating old backups for %s:' % project_name)
-      l = os.listdir(directory)
+    def rotate(self, count):
+      self.log.debug('Rotating old backups for %s:' % self.project_name)
+      l = os.listdir(self.project_dir)
       i = d = 0
       for f in sorted(l, reverse=True):
         if f != 'latest':
@@ -207,15 +218,15 @@ class bast(object):
       self.log.debug(status)
       return status
       
-    def notify(self, project_name, status, report):
-      self.log.debug('Sending notifications for %s.' % project_name)
+    def notify(self, status, report):
+      self.log.debug('Sending notifications for %s.' % self.project_name)
       import smtplib
       from email.mime.text import MIMEText
-      report.insert(0, 'Backup for %s status is: %s.' % (project_name, status))
+      report.insert(0, 'Backup for %s status is: %s.' % (self.project_name, status))
       report.insert(0, 'Hello!')
       body = "\n".join(report)
       msg = MIMEText(body)
-      msg['Subject'] = '[%s - Backup] %s' % (project_name, status)
+      msg['Subject'] = '[%s - Backup] %s' % (self.project_name, status)
       msg['From'] = self.config.get('BAST', 'mail.username')
       msg['To'] = self.config.get('BAST', 'mail.notify')
       s = smtplib.SMTP(self.config.get('BAST', 'mail.server'), self.config.get('BAST', 'mail.port'))
--- plugins/files.py
+++ plugins/files.py
@@ -1,17 +1,65 @@
+import os
 import sys
 import shutil
 from bt.plugin import BastPlugin
 
 class files(BastPlugin):
   
-  def run(self, directory):
+  def run(self, directory, symlinks='copy'):
     dir_name = self.backup_id + '--files'
     self.log.debug("Backing up directory %s." % directory)
+    if symlinks == 'follow':
+      symlinks_mode = 2
+    elif symlinks == 'copy':
+      symlinks_mode = 1
+    else:
+      symlinks_mode = 0
     try:
-      shutil.copytree(directory, dir_name, symlinks=True)
+      self.copytree(directory, dir_name, symlinks=symlinks_mode)
     except:
       self.log.error('File backup cancelled! (%s)' % sys.exc_info()[1])
     else:
       self.compress(dir_name)
       self.log.debug('Files backup complete.')
       self.status = True
+    
+  def copytree(self, src, dst, symlinks=0, ignore=None):
+    '''
+    A modified version of os.copytree to add more options like:
+    symlinks: ignore them totally
+    '''
+    names = os.listdir(src)
+    if ignore is not None:
+        ignored_names = ignore(src, names)
+    else:
+        ignored_names = set()
+    os.makedirs(dst)
+    errors = []
+    for name in names:
+      if name in ignored_names:
+          continue
+      srcname = os.path.join(src, name)
+      dstname = os.path.join(dst, name)
+      try:
+          if os.path.islink(srcname):
+            if symlinks == 1:
+              linkto = os.readlink(srcname)
+              os.symlink(linkto, dstname)
+            elif symlinks == 2:
+              shutil.copy2(srcname, dstname)
+          elif os.path.isdir(srcname):
+              shutil.copytree(srcname, dstname, symlinks, ignore)
+          else:
+              shutil.copy2(srcname, dstname)
+      except (IOError, os.error), why:
+          errors.append((srcname, dstname, str(why)))
+      # catch the Error from the recursive copytree so that we can
+      # continue with other files
+      except ValueError, err:
+          errors.extend(err.args[0])
+    try:
+        shutil.copystat(src, dst)
+    except OSError, why:
+        errors.extend((src, dst, str(why)))
+    if errors:
+        raise ValueError(errors)
